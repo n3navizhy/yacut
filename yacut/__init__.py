f85c2b223bbd202937199cf3c86a49f1a49488c3
n3navizhy <93191625+n3navizhy@users.noreply.github.com>
2022-11-29 20:39:28 +0300

    починил статику

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from settings import Config
 
-app = Flask(__name__,  static_folder='yacut/static')
+app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
